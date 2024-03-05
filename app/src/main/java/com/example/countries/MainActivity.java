f0b03aa971f7423c044d7419ca7954db0239e605
Akulik <Akulik03@icloud.com>
2024-03-05 11:34:42 +0200

    country_class

--- app/src/main/java/com/example/countries/MainActivity.java
+++ app/src/main/java/com/example/countries/MainActivity.java
@@ -34,23 +34,5 @@ public class MainActivity extends AppCompatActivity {
 
         CountryAdapter countryAdapter = new CountryAdapter(getApplicationContext(), countries);
         listView.setAdapter(countryAdapter);
-       // Bitmap[] flags = {getResizedBitmap(R.drawable.rus), getResizedBitmap(R.drawable.usa), getResizedBitmap(R.drawable.germany), getResizedBitmap(R.drawable.china), getResizedBitmap(R.drawable.moldova), getResizedBitmap(R.drawable.argentina), getResizedBitmap(R.drawable.france), getResizedBitmap(R.drawable.italy), getResizedBitmap(R.drawable.brazil), getResizedBitmap(R.drawable.canada)};
-
-
-        //ListView listView = findViewById(R.id.listView);
-
-        //String[] countries = getResources().getStringArray(R.array.countries);
-
-        // используем адаптер данных
-        //CountryAdapter countryAdapter = new CountryAdapter(getApplicationContext(), countries, flags);
-        //listView.setAdapter(countryAdapter);
-
     }
-    private Bitmap getResizedBitmap(int resourceId) {
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resourceId);
-        int width = (int) getResources().getDimension(R.dimen.flag_width);
-        int height = (int) getResources().getDimension(R.dimen.flag_height);
-        return Bitmap.createScaledBitmap(bitmap, width, height, false);
-    }
-
 }
