8f448556d1859291227a459c2363b71a382041e7
Akulik <Akulik03@icloud.com>
2024-03-05 11:26:23 +0200

    country_class

--- app/src/main/java/com/example/countries/CountryAdapter.java
+++ app/src/main/java/com/example/countries/CountryAdapter.java
@@ -2,6 +2,7 @@ package com.example.countries;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,42 +10,77 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class CountryAdapter extends BaseAdapter {
-    Context context;
-    String countries[];
-    Bitmap flags[];
-    LayoutInflater inflter;
+import java.util.List;
 
-    public CountryAdapter(Context context, String[] countries, Bitmap[] flags) {
+public class CountryAdapter extends BaseAdapter {
+    private Context context;
+    private List<Country> countries;
+    private LayoutInflater inflater;
+
+    public CountryAdapter(Context context, List<Country> countries) {
         this.context = context;
         this.countries = countries;
-        this.flags = flags;
-        inflter = LayoutInflater.from(context);
+        inflater = LayoutInflater.from(context);
     }
-
 
     @Override
     public int getCount() {
-        return countries.length;
+        return countries.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Object getItem(int position) {
+        return countries.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        view = inflter.inflate(R.layout.list_item, null);
-        TextView country = (TextView) view.findViewById(R.id.textView);
-        ImageView icon = (ImageView) view.findViewById(R.id.icon);
-        country.setText(countries[i]);
-        icon.setImageBitmap(flags[i]);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView countryName = view.findViewById(R.id.textView);
+        ImageView flagImage = view.findViewById(R.id.icon);
+
+        Country country = countries.get(position);
+        countryName.setText(country.getName());
+        Bitmap compressedBitmap = compressBitmap(country.getFlagId(), 140, 85);
+        flagImage.setImageBitmap(compressedBitmap);
+
         return view;
+    }
+
+    private Bitmap compressBitmap(int resourceId, int reqWidth, int reqHeight) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(context.getResources(), resourceId, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(context.getResources(), resourceId, options);
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
--- app/src/main/java/com/example/countries/MainActivity.java
+++ app/src/main/java/com/example/countries/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,16 +18,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bitmap[] flags = {getResizedBitmap(R.drawable.rus), getResizedBitmap(R.drawable.usa), getResizedBitmap(R.drawable.germany), getResizedBitmap(R.drawable.china), getResizedBitmap(R.drawable.moldova), getResizedBitmap(R.drawable.argentina), getResizedBitmap(R.drawable.france), getResizedBitmap(R.drawable.italy), getResizedBitmap(R.drawable.brazil), getResizedBitmap(R.drawable.canada)};
-
-
         ListView listView = findViewById(R.id.listView);
 
-        String[] countries = getResources().getStringArray(R.array.countries);
+        List<Country> countries = new ArrayList<>();
+        countries.add(new Country("Россия", R.drawable.rus));
+        countries.add(new Country("США", R.drawable.usa));
+        countries.add(new Country("Германия", R.drawable.germany));
+        countries.add(new Country("Китай", R.drawable.china));
+        countries.add(new Country("Молдова", R.drawable.moldova));
+        countries.add(new Country("Аргентина", R.drawable.argentina));
+        countries.add(new Country("Франция", R.drawable.france));
+        countries.add(new Country("Италия", R.drawable.italy));
+        countries.add(new Country("Бразилия", R.drawable.brazil));
+        countries.add(new Country("Канада", R.drawable.canada));
+
+        CountryAdapter countryAdapter = new CountryAdapter(getApplicationContext(), countries);
+        listView.setAdapter(countryAdapter);
+       // Bitmap[] flags = {getResizedBitmap(R.drawable.rus), getResizedBitmap(R.drawable.usa), getResizedBitmap(R.drawable.germany), getResizedBitmap(R.drawable.china), getResizedBitmap(R.drawable.moldova), getResizedBitmap(R.drawable.argentina), getResizedBitmap(R.drawable.france), getResizedBitmap(R.drawable.italy), getResizedBitmap(R.drawable.brazil), getResizedBitmap(R.drawable.canada)};
+
+
+        //ListView listView = findViewById(R.id.listView);
+
+        //String[] countries = getResources().getStringArray(R.array.countries);
 
         // используем адаптер данных
-        CountryAdapter countryAdapter = new CountryAdapter(getApplicationContext(), countries, flags);
-        listView.setAdapter(countryAdapter);
+        //CountryAdapter countryAdapter = new CountryAdapter(getApplicationContext(), countries, flags);
+        //listView.setAdapter(countryAdapter);
 
     }
     private Bitmap getResizedBitmap(int resourceId) {
